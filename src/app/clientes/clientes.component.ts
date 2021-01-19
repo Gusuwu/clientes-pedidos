f1471c476aeabfc982557e6594d970c4dd385906
Gusuwu <augustoadrian@hotmail.com>
2021-01-19 12:34:24 -0300

    commit ordenar

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatTable } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import {Cliente} from '../shared/cliente';
 import {ClienteService} from '../shared/cliente.service';
+import { ConfirmarComponent } from '../confirmar/confirmar.component';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
-export class ClientesComponent implements OnInit {
+export class ClientesComponent implements OnInit, AfterViewInit {
 
   clientes : Cliente [] = [];
   clienteSelected = new Cliente();
 
+  dataSource = new MatTableDataSource<Cliente>();
+
   mostrarForm = false;
   form = new FormGroup({});
 
-  constructor(private cS : ClienteService, private formBuilder: FormBuilder) { }
+  constructor(private cS : ClienteService, private formBuilder: FormBuilder, public dialog: MatDialog) { }
+  
+  @ViewChild(MatSort) sort!: MatSort;
+  
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
 
-  columnas : string[] = ['id', 'nombre', 'direccion', 'editar', 'borrar'];
+  columnas : string[] = ['clienId', 'clienNombre', 'clienDireccion', 'acciones'];
 
   ngOnInit(): void {
 
@@ -34,10 +46,15 @@ export class ClientesComponent implements OnInit {
     this.cS.get().subscribe(
       (clientes) => {
         this.clientes = clientes;
-        console.log(clientes);
+        this.actualizarTabla();
       } 
     )
 
+  }
+
+  actualizarTabla() {
+    this.dataSource.data = this.clientes;
+    this.dataSource.sort = this.sort;
   }
 
   agregar() {
@@ -47,16 +64,29 @@ export class ClientesComponent implements OnInit {
   }
 
   delete(row: Cliente) {
-    this.cS.delete(row.clienId)
-      .subscribe(() => {
-        this.clientes = this.clientes.filter((x) => {
-          if (x.clienId != row.clienId) {
-            return true
-          } else {
-            return false
-          }
-        });
-      });
+    const dialogRef = this.dialog.open(ConfirmarComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+
+      if (result) {
+        this.cS.delete(row.clienId)
+          .subscribe(() => {
+
+            //this.items = this.items.filter( x => x !== row);
+
+            this.clientes = this.clientes.filter((cliente) => {
+              if (cliente.clienId != row.clienId) {
+                return true
+              } else {
+                return false
+              }
+            });
+
+            this.actualizarTabla();
+          });
+      }
+    });
   }
 
   edit(seleccionado: Cliente) {
@@ -84,6 +114,7 @@ export class ClientesComponent implements OnInit {
         .subscribe((cliente) => {
           this.clientes.push(cliente);
           this.mostrarForm = false;
+          this.actualizarTabla();
         });
 
     }
